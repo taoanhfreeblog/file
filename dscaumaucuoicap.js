4beab3b0766390b5a28a9a7a5f54911654031a99
taoanhfreeblog <taoanhfreeblog@gmail.com>
2026-05-17 21:42:04 +0700

    Upload dscaumaucuoicap.js

---
+++ dscaumaucuoicap.js
@@ -0,0 +1,13 @@
+   const dscau = [
+   "Chúng ta từng nghĩ quãng thời gian cấp ba sẽ rất dài, nhưng đến hôm nay mới nhận ra mọi thứ trôi qua thật nhanh. Cảm ơn thầy cô và các bạn đã cùng em trải qua những ngày tháng ý nghĩa nhất của tuổi trẻ.",
+   "Sau hôm nay, mỗi người sẽ có một con đường riêng. Hy vọng rằng tất cả chúng ta đều sẽ trưởng thành theo cách mình mong muốn và luôn giữ được nụ cười của những ngày tuổi trẻ.",
+   "Mong rằng sau này dù có bận rộn thế nào, chúng ta vẫn sẽ nhớ về nhau như những người bạn từng đi cùng cả một thanh xuân. Chúc tất cả luôn mạnh mẽ, hạnh phúc và đạt được điều mình mong muốn.",
+   "Cảm ơn vì đã xuất hiện trong quãng thời gian đẹp nhất của tuổi học trò. Mong rằng sau này gặp lại, chúng ta đều đã trở thành phiên bản tốt hơn của chính mình.",
+   "Mai này có thể không còn gặp nhau thường xuyên nữa, nhưng mong rằng mỗi khi nhắc về lớp mình, ai cũng sẽ mỉm cười vì đã từng có một thanh xuân thật đẹp.",
+   "Mong rằng sau khi rời khỏi mái trường này, mỗi người đều sẽ tìm được con đường phù hợp với mình và luôn sống hết mình với tuổi trẻ",
+   "Chúc cho tất cả chúng ta sau này đều có công việc mình yêu thích, cuộc sống mình mong muốn và luôn nhớ rằng đã từng có một thời thanh xuân rất đẹp bên nhau.",
+   "Có lẽ sau hôm nay, sẽ rất khó để có thể đông đủ như lúc này nữa. Cảm ơn vì đã cùng nhau đi qua những ngày vui buồn của tuổi học trò. Chúc tất cả chúng ta sẽ luôn thành công và hạnh phúc trên con đường riêng.",
+   "Hôm nay mình mới thật sự cảm nhận được rằng tuổi học trò sắp kết thúc. Những điều từng nghĩ là bình thường như tiếng trống trường hay giờ ra chơi giờ lại trở thành điều đáng nhớ nhất. Cảm ơn tất cả vì một thanh xuân thật đẹp.",
+   "Chúng ta rồi sẽ trưởng thành, sẽ có những cuộc sống riêng và những ước mơ riêng. Nhưng chắc chắn rằng quãng thời gian ngồi cùng lớp, cùng cười đùa dưới mái trường này sẽ mãi là ký ức không thể thay thế.",
+   "Điều đẹp nhất của tuổi học trò không chỉ là những thành tích đạt được, mà còn là những người bạn đã cùng mình đi qua cả một quãng thanh xuân. Cảm ơn tất cả vì đã xuất hiện trong những năm tháng đẹp nhất ấy."
+   ;
